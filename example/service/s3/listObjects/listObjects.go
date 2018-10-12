cf826d8a2ee8f89dc0a7ca6b4416785e19df51ba
iliul <liul.stone@gmail.com>
2018-10-12 16:57:35 +0800

    Add SpeedyCloud ListObject Interface example
    Signed-off-by: iliul <liul.stone@gmail.com>

--- example/service/s3/listObjects/listObjects.go
+++ example/service/s3/listObjects/listObjects.go
@@ -6,8 +6,16 @@ import (
 	"fmt"
 	"os"
 
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/credentials"
 	"github.com/aws/aws-sdk-go/aws/session"
 	"github.com/aws/aws-sdk-go/service/s3"
+)
+
+const (
+	ACCESS_KEY                     = "FIX_ME"
+	SECRET_KEY                     = "FIX_ME"
+	SPEEDYCLOUD_OS_ENDPOINT string = "https://oss-cn-shanghai.speedycloud.org"
 )
 
 // Lists all objects in a bucket using pagination
@@ -20,11 +28,23 @@ func main() {
 		return
 	}
 
-	sess := session.Must(session.NewSession())
+	// 构建配置项
+	cfg := &aws.Config{
+		Region:                        aws.String("us-east-1"), //By default, Don't edit it
+		Credentials:                   credentials.NewStaticCredentials(ACCESS_KEY, SECRET_KEY, ""),
+		Endpoint:                      aws.String(SPEEDYCLOUD_OS_ENDPOINT),
+		S3DisableContentMD5Validation: aws.Bool(true),
+		S3ForcePathStyle:              aws.Bool(true),
+	}
 
+	// 实例化 Session 对象
+	sess := session.Must(session.NewSession(cfg))
+
+	// 实例化 Service Client
 	svc := s3.New(sess)
 
 	i := 0
+	// 调用 ListObjectsPages 接口查询对象列表
 	err := svc.ListObjectsPages(&s3.ListObjectsInput{
 		Bucket: &os.Args[1],
 	}, func(p *s3.ListObjectsOutput, last bool) (shouldContinue bool) {
